308047d312e88aa7f7b9a9fd7fa16cde9eb7eece
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-30 22:27:53 -0700

    Make Shopping List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/header/header.component';
+import { ShoppingListEditComponent } from 'src/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingListComponent } from 'src/shopping-list/shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
