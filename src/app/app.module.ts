b51ba5e25455df01eed44f1060c34cbfc04a604d
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-30 23:13:38 -0700

    File System sucked so trying to restart.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from 'src/header/header.component';
-import { ShoppingListEditComponent } from 'src/shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { ShoppingListComponent } from 'src/shopping-list/shopping-list/shopping-list.component';
-import { RecipeListComponent } from 'src/recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from 'src/recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from 'src/recipe-book/recipe-detail/recipe-detail.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ShoppingListComponent,
-    ShoppingListEditComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
