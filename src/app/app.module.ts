13e39b3bfc9281671bc7f5dd2f12884177e2d9bf
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-12-30 22:36:56 -0700

    Add Recipe List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from 'src/header/header.component';
 import { ShoppingListEditComponent } from 'src/shopping-list/shopping-list-edit/shopping-list-edit.component';
 import { ShoppingListComponent } from 'src/shopping-list/shopping-list/shopping-list.component';
 import { RecipeListComponent } from 'src/recipe-book/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from 'src/recipe-book/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,9 @@ import { RecipeListComponent } from 'src/recipe-book/recipe-list/recipe-list.com
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipeListComponent
+    RecipeListComponent,
+    RecipeItemComponent,
+    
   ],
   imports: [
     BrowserModule
